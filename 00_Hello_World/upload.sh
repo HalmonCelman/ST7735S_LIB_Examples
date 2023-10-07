b14a751aa9352262b7568d374a84b48e735d6f16
HalmonCelman <halmoncelman@gmail.com>
2023-10-07 17:44:52 +0200

    upload.sh corected

--- 00_Hello_World/upload.sh
+++ 00_Hello_World/upload.sh
@@ -1,1 +1,1 @@
-openocd -f interface/stlink-v2.cfg -f target/stm32f3x.cfg -c "program build/Example1 verify reset exit"
+openocd -f interface/stlink-v2.cfg -f target/stm32f3x.cfg -c "program build/Example0 verify reset exit"
